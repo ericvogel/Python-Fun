92df67eb84b761ee08a47c2532f1966d67df1ff5
ericvogel <lolking@mailinator.com>
2014-08-01 13:15:29 -0400

    Partial image support

--- Main.py
+++ Main.py
@@ -1,59 +1,42 @@
 from Tkinter import *
 import time
+import LineObject
+import WorldData
+
+
+
+master = Tk()
+w = Canvas(master, width = WorldData.getWorldWidth(), height = WorldData.getWorldHeight())
+w.pack()
+
+
+def getW():
+	return w
 
 def Run():
 	frameRate = 30
 	ObjectsList = []
-
-	class LineObject():
-		x = 0
-		y = 0
-		movingRight = True
-		movingSpeed = 4
-
-		def __init__(self):
-			pass
-
-		def setPos(self, newX, newY):
-			self.x = newX
-			self.y = newY
-		def getX(self):
-			return (self.x)
-		
-		def getY(self):
-			return (self.y)
-		def draw(self):
-			w.create_line(line1.getX(), line1.getY(), line1.getX(), line1.getY()+100, width=2)
-
-		def update(self):
-			if ((self.getX() < 799) and (self.movingRight == True)):
-				self.setPos(self.getX()+self.movingSpeed, self.getY())
-			elif (self.getX() >= 799):
-				self.setPos(self.getX()-self.movingSpeed, self.getY())
-				self.movingRight = False
-			elif ((self.getX() > 1) and (self.movingRight == False)):
-				self.setPos(self.getX()-self.movingSpeed, self.getY())
-			elif ((self.getX() <= 0)):
-				self.setPos(self.getX()+self.movingSpeed, self.getY())
-				self.movingRight = True
-			else:
-				print'Error wrong state reached for ' + str(self)
-		
-
+	global w
+	global master
 
 	def clearScreen():
 		w.delete(ALL)
 
 
-	line1 = LineObject()
+	line1 = LineObject.LineObject()
+	line1.setDraw(w.create_line(line1.getX(), line1.getY(), line1.getX(), line1.getY() + 200, width=2))
 	line1.setPos(10, 10)
 	ObjectsList.append(line1)
 	
+	
+
+
+
 	def drawObjects():
 		#print 'drawing objects'
 		
 		for objectToDraw in ObjectsList: 
-			objectToDraw.draw()
+			objectToDraw.getDraw()
 
 
 	def updateObjects():
@@ -67,14 +50,18 @@ def Run():
 	def pressedDown(event):
 		print 'pressed Down'
 
+	def releasedUp(event):
+		print 'released up'
 
-	master = Tk()
-	w = Canvas(master, width=800, height=600)
-	w.pack()
+	def releasedDown(event):
+		print 'released Down'
+
+
+	
 	master.bind('<Up>', pressedUp)
 	master.bind('<Down>', pressedDown)
-	master.bind('<Up-KeyRelease>', releasedUp)
-	master.bind('<Down-KeyRelease>', releasedDown)
+	master.bind('<KeyRelease-Up>', releasedUp)
+	master.bind('<KeyRelease-Down>', releasedDown)
 
 	#timeOne = time.time()
 	#print timeOne
---
+++ WorldData.py
@@ -0,0 +1,11 @@
+worldWidth = 800
+worldHeight = 600
+def __init__():
+	worldWidth = 800
+	worldHeight = 600
+
+def getWorldWidth():
+	return worldWidth
+
+def getWorldHeight():
+	return worldHeight
